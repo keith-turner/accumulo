044e3779a2c09e4027d73220bd9777a4910db3ba
Keith Turner <kturner@apache.org>
2020-03-09 19:18:29 -0400

    WIP

---
+++ core/src/main/java/org/apache/accumulo/core/spi/compaction/CompactionJob.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.accumulo.core.spi.compaction;
+
+import java.net.URI;
+import java.util.Collection;
+
+public class CompactionJob {
+
+  private final int priority;
+  private final String executor;
+  private final Collection<URI> files;
+  // TODO compaction config
+
+  public CompactionJob(int priority, String executor, Collection<URI> files) {
+    this.priority = priority;
+    this.executor = executor;
+    this.files = files;
+  }
+}
