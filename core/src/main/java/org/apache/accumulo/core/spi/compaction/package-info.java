851d531f8b177ce89b6b22e5c5ad2d1eb43f0cae
Keith Turner <kturner@apache.org>
2020-04-16 18:15:30 -0400

    WIP

--- core/src/main/java/org/apache/accumulo/core/spi/compaction/package-info.java
+++ core/src/main/java/org/apache/accumulo/core/spi/compaction/package-info.java
@@ -18,8 +18,9 @@
  */
 /**
  * This package provides a place for plugin interfaces related to executing compactions. The diagram
- * below show the functional components in Accumulo related compactions. Not all of these components
- * are pluggable, but understanding how everything fits together is important for writing a plugin.
+ * below shows the functional components in Accumulo related to compactions. Not all of these
+ * components are pluggable, but understanding how everything fits together is important for writing
+ * a plugin.
  *
  * <p>
  * <img src="doc-files/compaction-spi-design.png"/>
@@ -28,23 +29,41 @@
  * The following is a desciption of each functional component.
  *
  * <ul>
- * <li><b>Compaction Manager</b> A non pluggable component within the tablet server that manages
- * compactions and brings all other components together. It routes compactables to compaction
- * services.
- * <li><b>Compaction Service</b> A non pluggable component within a Compaction Manager that compacts
- * tablets. One or more of these are created based on user configuration. Users can assign a table
- * to a compaction service. Has a single compaction planner and one ore more compaction executors.
+ * <li><b>Compaction Manager</b> A non pluggable component within the tablet server that brings all
+ * other components together.
+ * <ul>
+ * <li><b>Compaction Service</b> A non pluggable component that compacts tablets. One or more of
+ * these are created based on user configuration. Users can assign a table to a compaction service.
+ * Has a single compaction planner and one ore more compaction executors.
+ * <ul>
  * <li><b>Compaction Planner</b> A pluggable component that can be configured by users when they
  * configure a compaction service. It makes decisions about which files to compact on which
  * executors. See {@link org.apache.accumulo.core.spi.compaction.CompactionPlanner}
- * <li><b>Compaction Executor</b> A non pluggable component that executes compactions and has a
- * priority queue.
- * <li><b>Compaction Dispatcher</b> A pluggable component that decides which compaction service a
- * table should use for different kinds of compactions. This is configurable by users per table. See
- * {@link org.apache.accumulo.core.spi.compaction.CompactionDispatcher}
- * <li><b>Compactable</b> A non pluggable component that wraps a Tablet and a compaction dispatcher.
- * It tracks all information about one or more running compactions that is needed by a compaction
- * service.
+ * <li><b>Compaction Executor</b> A non pluggable component that executes compactions using multiple
+ * threads and has a priority queue.</li>
+ * </ul>
+ * </li>
+ * <li><b>Compactable</b> A non pluggable component that wraps a Tablet and per table pluggable
+ * compaction components. It tracks all information about one or more running compactions that is
+ * needed by a compaction service in a thread safe manor.
+ * <ul>
+ * <li><b>Compaction Dispatcher</b> A pluggable component component that decides which compaction
+ * service a table should use for different kinds of compactions. This is configurable by users per
+ * table. See {@link org.apache.accumulo.core.spi.compaction.CompactionDispatcher}</li>
+ * <li><b>Compaction Selector</b> A pluggable component that can optionally be configured per table
+ * to periodically select files to compact. This support use cases like periodically compacting all
+ * files because there are too many deletes. See
+ * {@link org.apache.accumulo.core.client.admin.compaction.CompactionSelector}</li>
+ * <li><b>Compaction Configurer</b> A pluggable component that can optionally be configured per
+ * table to dynamically configure file output settings. This support use cases like user snappy for
+ * small files and gzip for large files. See
+ * {@link org.apache.accumulo.core.client.admin.compaction.CompactionConfigurer}</li>
+ * <li><b>Compaction Strategy</b> A deprecated pluggable component replaced by the Selector and
+ * Configurer. If configured it will be used.
+ * </ul>
+ * </li>
+ * </ul>
+ * </li>
  * </ul>
  *
  *
