d622e93f57ca498ab9061550e4362c10801748e4
Keith Turner <kturner@apache.org>
2020-04-28 17:49:11 -0400

    WIP bug fix

--- server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/CompactableUtils.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/CompactableUtils.java
@@ -141,7 +141,7 @@ public class CompactableUtils {
   }
 
   static CompactionPlan selectFiles(CompactionKind kind, Tablet tablet,
-      SortedMap<StoredTabletFile,DataFileValue> datafiles, CompactionStrategyConfig cscˇ) {
+      SortedMap<StoredTabletFile,DataFileValue> datafiles, CompactionStrategyConfig csc) {
 
     var trsm = tablet.getTabletResources().getTabletServerResourceManager();
 
