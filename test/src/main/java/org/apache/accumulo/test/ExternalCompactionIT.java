957d2a27bb5d3edd14724759d9a1aacf7834a38f
Dave Marion <dlmarion@apache.org>
2021-04-27 16:19:19 +0000

    Add missing import statement

--- test/src/main/java/org/apache/accumulo/test/ExternalCompactionIT.java
+++ test/src/main/java/org/apache/accumulo/test/ExternalCompactionIT.java
@@ -22,6 +22,7 @@ import static org.apache.accumulo.minicluster.ServerType.TABLET_SERVER;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.net.URI;
